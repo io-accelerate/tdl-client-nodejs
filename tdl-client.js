1625599faccc33bd174e9667ec867eec70777654
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-03-28 09:21:32 +0000

    feat: all tests passing

--- tdl-client.js
+++ tdl-client.js
@@ -1,5 +1,5 @@
 exports.ProcessingRules = require("./lib/queue/processing_rules");
-exports.AuditStream = require("./lib/audit/audit_stream");
+exports.AuditStream = require("./lib/audit/presentation_utils");
 exports.ImplementationRunnerConfig = require("./lib/queue/implementation_runner_config.js");
 exports.QueryBasedImplementationRunner = require("./lib/queue/queue_based_implementation_runner");
 exports.QueryBasedImplementationRunnerBuilder = require("./lib/queue/queue_based_implementation_runner_builder");
